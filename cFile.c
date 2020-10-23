0a4d3e49ffb9912ab94ad37e59d291b2ddc6f6bf
Siddhant Kumar <siddhantkumarupmanyu@gmail.com>
2020-10-23 14:28:57 +0530

    remove copy

--- cFile.c
+++ cFile.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-    char const *const fileName = argv[1]; /* should check that argc > 1 */
-    FILE* file = fopen(fileName, "r");    /* should check the result */
-    char line[256];
+    char const *const fileName = argv[1]
+    FILE* file = fopen(fileName, "r") 
+    char line[256]
 
     while (fgets(line, sizeof(line), file)) {
         /* note that fgets don't strip the terminating \n, checking its
            presence would allow to handle lines longer that sizeof(line) */
-        printf("%s", line);
+        printf("%s", line)
     }
     /* may check feof here to make a difference between eof and io failure -- network
        timeout for instance */
 
-    printf("\n");
+    printf("\n")
 
-    fclose(file);
+    fclose(file)
 
-    return 0;
+    return 0
 }
--- formatedCFile.c
+++ formatedCFile.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 int main(int argc, char* argv[]) {
-    char const *const fileName = argv[1]; /* should check that argc > 1 */
-    FILE* file = fopen(fileName, "r");    /* should check the result */
+    char const *const fileName = argv[1];
+    FILE* file = fopen(fileName, "r") ;
     char line[256];
 
     while (fgets(line, sizeof(line), file)) {
-        /* note that fgets don't strip the terminating \n, checking its
+        /* note that fgets don't strip the terminating \n, checking its;
            presence would allow to handle lines longer that sizeof(line) */
         printf("%s", line);
     }
-    /* may check feof here to make a difference between eof and io failure -- network
+    /* may check feof here to make a difference between eof and io failure -- network;
        timeout for instance */
 
     printf("\n");
